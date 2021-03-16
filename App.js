3b550bdd25e92a9eefb004e441e55d7a2865b413
Rayhan133 <rayhanalshorif@gmail.com>
2021-03-16 16:52:16 +0600

    Tesla Clone Apps Using React Native

--- App.js
+++ App.js
@@ -1,19 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, ImageBackground } from 'react-native';
-import CarItem from './assets/components/CarItem';
+import { StyleSheet, View, } from 'react-native';
+import CarsList from './assets/components/CarsList';
+import Header from './assets/components/Header';
+
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <CarItem />
-    </View >
+      <CarsList />
+      <Header />
+      <StatusBar style='auto' />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
--- assets/components/CarItem/index.js
+++ assets/components/CarItem/index.js
@@ -1,22 +1,56 @@
+
 import React from 'react';
 import { View, Text, ImageBackground } from 'react-native';
 import styles from './styles';
+import StyledButton from '../StyledButton';
+
+
 
 const CarItem = (props) => {
-    return (
 
+    const { name, tagLine, tagLineCTA, image } = props.car;
+
+
+
+    return (
         <View style={styles.carContainer}>
-            <ImageBackground source={require('../../images/ModelS.jpeg')}
+            <ImageBackground
+                source={image}
                 style={styles.image}
             />
             <View style={styles.titles}>
-                <Text style={styles.title}>Model S</Text>
-                <Text style={styles.subtitle}>Starting at $69,420</Text>
-            </View>
-        </View>
+                <Text style={styles.title}>{name}</Text>
+                <Text style={styles.subtitle}>{tagLine}
+                    {' '}
+                    <Text style={styles.subtitleCTA}>{tagLineCTA}</Text>
+                </Text>
 
+            </View>
+            <View style={styles.btnContainer}>
+                <StyledButton
+                    type="primary"
+                    content={"Custom Order"}
+                    onPress={() => {
+                        console.warn('Custom Order was Pressed')
+                    }} />
+
+                <StyledButton
+                    type="secondary"
+                    content={"Existing Inventory"}
+                    onPress={() => {
+                        console.warn("Existing Inventory was Pressed")
+                    }} />
+            </View>
+
+        </View>
     );
 
-};
+}
 
+
+
+//make this component available to the app
 export default CarItem;
+
+
+
